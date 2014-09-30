860afdb4b5cda7875d7b306cbe1d50e48bbd2260
gomenasaai <contramundee@gmail.com>
2014-09-30 01:06:05 +0100

    added server side error handling

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,15 +8,27 @@
 module.exports = {
 	
 	'new': function(req, res){
+		// available to our views; without _.clone its just a pointer (we need our own copy)
+		res.locals.flash = _.clone(req.session.flash);
 		res.view();
+		req.session.flash = {};
 	},
 
 	'create': function(req, res, next){
 
 		User.create(req.params.all(), function userCreated(err, user){
-			if (err) return next(err);
+			if (err) {
+				console.log(err);
+				// not available to our views
+				req.session.flash = {
+					err: err
+				}
+
+				return res.redirect('/user/new');
+			}
 
 			res.json(user);
+			req.session.flash = {};
 		});
 
 	}
