942fea8c1a8b6343ab750b25de18ffc2889cc455
gomenasaai <contramundee@gmail.com>
2014-09-30 15:32:40 +0100

    can now edit as well as delete users

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -39,6 +39,49 @@ module.exports = {
 				user: user
 			});
 		});
+	},
+
+	index: function(req, res, next){
+		User.find(function foundUsers(err, users){
+			if(err) return next(err);
+			res.view({
+				users: users
+			});
+		});
+	},
+
+	edit: function(req, res, next){
+		User.findOne(req.param('id'), function foundUser(err, user){
+			if(err) return next(err);
+			if(!user) return next('User doesnt exist');
+
+			res.view({
+				user: user
+			});
+		});
+	},
+
+	update: function(req, res, next){
+		User.update(req.param('id'), req.params.all(), function userUpdated(err){
+			if(err){
+				return res.redirect('/user/edit/' + req.param('id'));
+			}
+
+			res.redirect('/user/show/' + req.param('id'));
+		});
+	},
+
+	destroy: function(req, res, next){
+		User.findOne(req.param('id'), function foundUser(err, user){
+			if(err) return next(err);
+			if(!user) return next('User doesnt exist');
+
+			User.destroy(req.param('id'), function userDestroyed(err){
+				if(err) return next(err);
+			});
+
+			res.redirect('/user');
+		})
 	}
 
 };
