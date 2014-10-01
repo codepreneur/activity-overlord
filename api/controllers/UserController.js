affdd7f0e56956f08f59b9aa64b19900e399cf85
gomenasaai <contramundee@gmail.com>
2014-10-01 02:51:48 +0100

    properly implemented authentication with policies

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -42,10 +42,6 @@ module.exports = {
 	},
 
 	index: function(req, res, next){
-
-		console.log(new Date());
-		console.log(req.session.authenticated);
-		
 		User.find(function foundUsers(err, users){
 			if(err) return next(err);
 			res.view({
