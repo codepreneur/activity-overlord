790118de43c8789166852c2ad2ae82476dc23588
gomenasaai <contramundee@gmail.com>
2014-10-01 02:01:53 +0100

    added authentication (sign in) and cookies

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -42,6 +42,10 @@ module.exports = {
 	},
 
 	index: function(req, res, next){
+
+		console.log(new Date());
+		console.log(req.session.authenticated);
+		
 		User.find(function foundUsers(err, users){
 			if(err) return next(err);
 			res.view({
